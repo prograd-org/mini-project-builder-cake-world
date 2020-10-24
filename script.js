9f1d8e99f9c09afcc1d093e815463e4483a54549
abhid07 <a.deshpande4509@gmail.com>
2020-10-25 00:36:51 +0530

    Updated

--- script.js
+++ script.js
@@ -18,19 +18,19 @@ var bill = 0
 var chocBill = 0
 var strawBill = 0
 var butterBill = 0
-var vanBill =0
+var vanBill = 0
 var redBill = 0
 
 function addChocolate() {
     // document.getElementById('chocBtn').disabled=true
 
-    chocBill+=300
-    bill+=300
-    chocCost.innerHTML ="Chocolate ............................."+chocBill
+    chocBill += 300
+    bill += 300
+    chocCost.innerHTML = "Chocolate ................. " + chocBill
     console.log(chocBill)
-    
+
     layer++;
-    
+
     switch (layer) {
         case 1:
             layer1.style.visibility = 'visible';
@@ -43,7 +43,7 @@ function addChocolate() {
         case 3:
             layer3.style.visibility = 'visible';
             layer3.style.background = 'brown';
-           
+
             break;
         case 4:
             layer4.style.visibility = 'visible';
@@ -52,22 +52,22 @@ function addChocolate() {
         case 5:
             layer5.style.visibility = 'visible';
             layer5.style.background = 'brown';
-            candle.style.visibility='visible';
+            candle.style.visibility = 'visible';
             break;
 
         default:
             alert("Sorry....Maximum only 5 layers can be added!")
     }
     console.log(bill);
-    
+
 }
 
 function addStrawberry() {
-    bill+=100
-    strawBill+=100
+    bill += 100
+    strawBill += 100
     layer++;
-    strawCost.innerHTML ="Strawberry ..................... "+strawBill
-   
+    strawCost.innerHTML = "Strawberry ................. " + strawBill
+
     switch (layer) {
         case 1:
             layer1.style.visibility = 'visible';
@@ -76,55 +76,55 @@ function addStrawberry() {
         case 2:
             layer2.style.visibility = 'visible';
             layer2.style.background = '#fc5a8d';
-            
+
             break;
         case 3:
             layer3.style.visibility = 'visible';
             layer3.style.background = '#fc5a8d';
-            
+
             break;
         case 4:
             layer4.style.visibility = 'visible';
             layer4.style.background = '#fc5a8d';
-           
+
             break;
         case 5:
             layer5.style.visibility = 'visible';
             layer5.style.background = '#fc5a8d';
-            candle.style.visibility='visible';
+            candle.style.visibility = 'visible';
             break;
         default:
             alert("Sorry....Maximum only 5 layers can be added!")
     }
-    
+
     console.log(bill);
 }
 
 function addButterscotch() {
-    bill+=200
-    butterBill +=200
+    bill += 200
+    butterBill += 200
     layer++;
-    butterCost.innerHTML = "ButterScotch ................ " + butterBill
+    butterCost.innerHTML = "ButterScotch ................. " + butterBill
     switch (layer) {
         case 1:
             layer1.style.visibility = 'visible';
             layer1.style.background = '#D48827';
-            
+
             break;
         case 2:
             layer2.style.visibility = 'visible';
             layer2.style.background = '#D48827';
-           
+
             break;
         case 3:
             layer3.style.visibility = 'visible';
             layer3.style.background = '#D48827';
-            
+
             break;
         case 4:
             layer4.style.visibility = 'visible';
             layer4.style.background = '#D48827';
-            
+
             break;
         case 5:
             layer5.style.visibility = 'visible';
@@ -139,10 +139,10 @@ function addButterscotch() {
 }
 
 function addVannila() {
-    bill+=250;
-    vanBill+=250;
+    bill += 250;
+    vanBill += 250;
     layer++;
-    vanCost.innerHTML = "Vannila ........................................... " + vanBill
+    vanCost.innerHTML = "Vannila ................. " + vanBill
     switch (layer) {
         case 1:
             layer1.style.visibility = 'visible';
@@ -169,14 +169,14 @@ function addVannila() {
             alert("Sorry....Maximum only 5 layers can be added!")
     }
     console.log(bill);
-    
+
 }
 
 function addRedvelvet() {
-    bill+=350;
-    redBill+=350
+    bill += 350;
+    redBill += 350
     layer++;
-    redCost.innerHTML = "RedVelvet .............................. " + redBill
+    redCost.innerHTML = "RedVelvet ................. " + redBill
     switch (layer) {
         case 1:
             layer1.style.visibility = 'visible';
@@ -205,39 +205,27 @@ function addRedvelvet() {
     console.log(bill);
 }
 
-function buy()
-{
-    if (layer == 0)
-    {
-        alert("Please add some flaours")
-    }
-    else
-    {
-    document.getElementById('chocBtn').disabled=true;
+function buy() {
+    document.getElementById('chocBtn').disabled = true;
     document.getElementById('strawBtn').disabled = true;
     document.getElementById('butterBtn').disabled = true;
     document.getElementById('vanBtn').disabled = true;
     document.getElementById('redBtn').disabled = true;
 
     candle.style.visibility = 'visible'
-    if(layer == 1)
-    {
-        candle.style.top='55%'    
+    if (layer == 1) {
+        candle.style.top = '55%'
     }
-    else if(layer==2)
-    {
-        candle.style.top = '46%' 
+    else if (layer == 2) {
+        candle.style.top = '46%'
     }
-    else if(layer==3)
-    {
+    else if (layer == 3) {
         candle.style.top = '38%'
     }
-    else if (layer == 4) 
-    {
+    else if (layer == 4) {
         candle.style.top = '30%'
     }
-    
+
     var total = document.getElementById('total')
-    total.innerHTML="TOTAL ....................................... "+bill
-    }
+    total.innerHTML = "TOTAL ...................... " + bill
 }
